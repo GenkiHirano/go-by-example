40ed84f83eeeaf6e40163d3808d2a65602d503d8
GenkiHirano <genki86web@gmail.com>
2022-01-16 19:06:25 +0900

    Go by Example

--- files/writing-files.go
+++ files/writing-files.go
@@ -1,45 +1,42 @@
 package files
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+// func check(e error) {
+// 	if e != nil {
+// 		panic(e)
+// 	}
+// }
 
 func Task59() {
 	fmt.Println()
 
-	d1 := []byte("hello\ngo\n")
-	err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
-	check(err)
+	// d1 := []byte("hello\ngo\n")
+	// err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+	// check(err)
 
-	f, err := os.Create("/tmp/dat2")
-	check(err)
+	// f, err := os.Create("/tmp/dat2")
+	// check(err)
 
-	defer f.Close()
+	// defer f.Close()
 
-	d2 := []byte{115, 111, 109, 101, 10}
-	n2, err := f.Write(d2)
-	check(err)
-	fmt.Printf("wrote %d bytes\n", n2)
+	// d2 := []byte{115, 111, 109, 101, 10}
+	// n2, err := f.Write(d2)
+	// check(err)
+	// fmt.Printf("wrote %d bytes\n", n2)
 
-	n3, err := f.WriteString("writes\n")
-	check(err)
-	fmt.Printf("wrote %d bytes\n", n3)
+	// n3, err := f.WriteString("writes\n")
+	// check(err)
+	// fmt.Printf("wrote %d bytes\n", n3)
 
-	f.Sync()
+	// f.Sync()
 
-	w := bufio.NewWriter(f)
-	n4, err := w.WriteString("buffered\n")
-	check(err)
-	fmt.Printf("wrote %d bytes\n", n4)
+	// w := bufio.NewWriter(f)
+	// n4, err := w.WriteString("buffered\n")
+	// check(err)
+	// fmt.Printf("wrote %d bytes\n", n4)
 
-	w.Flush()
+	// w.Flush()
 }
